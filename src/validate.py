d93108af0562cf9474f4b44d2dd4d77afa2fed6d
Christopher Morrison <Chris.Morrison500@gmail.com>
2019-07-31 18:10:53 -0400

    Update validation script

--- src/validate.py
+++ src/validate.py
@@ -1,18 +1,14 @@
-#!/usr/bin/python3
-
+#!usr/bin/env python3
 # Validates the directory structure of a given problem set
-
 import os
 import sys
 import argparse
+import zipfile
 
 def validate_ctf_directory(directory, verbose=False):
     valid_directory = True
 
     categories = [folder for folder in os.listdir(directory) if os.path.isdir(f"{directory}/{folder}")]
-    # for dirname, subdirList, fileList in os.walk(directory, topdown=False):
-    #     if subdirList:
-    #         continue
 
     for category in categories:
         for challenge in os.listdir(f"{directory}/{category}"):
@@ -22,29 +18,38 @@ def validate_ctf_directory(directory, verbose=False):
 
                 # Standard
                 if "challenge.zip" not in fileList and verbose:
-                    print(f"(optional) challenge.zip missing in {dirname}")
-                    #valid_directory = False
+                    print(f"{dirname}: (optional) challenge.zip missing")
+
                 if "value.txt" not in fileList:
-                    print(f"value.txt missing in {dirname}")
+                    print(f"{dirname}: value.txt missing")
                     valid_directory = False
+
                 if "message.txt" not in fileList:
-                    print(f"message.txt missing in {dirname}")
+                    print(f"{dirname}: message.txt missing")
                     valid_directory = False
+
                 if "flag.txt" not in fileList:
-                    print(f"flag.txt missing in {dirname}")
+                    print(f"{dirname}: flag.txt missing")
                     valid_directory = False
                 
                 # Service-req
+                if "requires-server" in fileList:
+                    if "server.zip" not in fileList
+                        print(f"{dirname}: server.zip missing when requires-server specified")
+                        valid_directory = False
+                    #TODO: assert requires-server challenge in server.zip
+                    if open(f"{dirname}/requires-server").read().strip() not in zipfile.ZipFile(f"{dirname}/server.zip").namelist():
+                        print(f"{dirname}: requires-server lists a binary not in server.zip")
+                        valid_directory = False
+                # TODO: v2 Container-req
 
-                # Container-req
-
-                # Machine-req
+                # TODO: v2Machine-req
 
     if not valid_directory:
-        print(f"Validation attempt failed for {directory}")
+        print(f"{directory} is a valid challenge pack")
         return(1)
     else:
-        print(f"Validation attempt passed for {directory}")
+        print(f"{directory} is NOT a valid challenge pack")
         return(0)
 
 if __name__=="__main__":
